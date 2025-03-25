40bde642ca2b60f3777da0893f64a53bd75fdc87
Cristian Manuel Abalos Fernandez <cristianmanuel007@gmail.com>
2025-03-25 13:52:32 -0400

    reporte de citas
    ya se muestra la informacion segun base de datos

---
+++ app/Livewire/ReportAppointment.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\History;
+use Carbon\Carbon;
+use Livewire\Component;
+
+class ReportAppointment extends Component
+{
+
+    public $date_start;
+    public $date_end;
+
+    public function mount(){
+        $this->date_start = Carbon::now()->toDateString();
+        $this->date_end = Carbon::now()->toDateString();
+    }
+
+    public function render()
+    {
+        $data = History::whereHas('reservation',function($query){
+            $query->where('date','>=',$this->date_start)->where('date','<=',$this->date_end);
+        })->get();
+        $total = 0;
+        return view('livewire.report-appointment',compact('data','total'));
+    }
+}
---
+++ resources/views/livewire/report-appointment.blade.php
@@ -0,0 +1,43 @@
+<div>
+    <div class="d-flex justify-content-end">
+        <label class="col-form-label">Desde</label>
+        <input wire:model="date_start" type="date" class="form-control">
+        <label class="col-form-label">Hasta</label>
+        <input wire:model="date_end" type="date" class="form-control">
+        <button class="btn btn-primary"><i class="fa fa-search"></i></button>
+    </div>
+    <table class="table table-striped">
+        <thead>
+            <th>Fecha</th>
+            <th>Paciente</th>
+            <th>Medico</th>
+            <th>Descripción</th>
+            <th>Costo</th>
+            <th></th>
+        </thead>
+        <tbody>
+            @foreach ($data as $item)
+            @php
+                $total += $item->canceled;
+            @endphp
+            <tr>
+                <td>{{$item->reservation->date}}</td>
+                <td>{{$item->person->surname.' '. $item->person->name}}</td>
+                <td>{{$item->medic->surname.' '.$item->medic->name}}</td>
+                <td>{{$item->diagnostic->description}}</td>
+                <td>{{$item->canceled}}</td>
+                <td>
+                    <a href="#" class="btn btn-primary"><i class="fa fa-eye"></i></a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+        <tfoot>
+            <tr>
+                <td colspan="3"></td>
+                <td><strong>TOTAL</strong></td>
+                <td>{{$total}}</td>
+            </tr>
+        </tfoot>
+    </table>
+</div>
